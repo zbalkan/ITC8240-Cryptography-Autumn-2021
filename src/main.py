d706b0e50bd502f27ba908980a4df195e068f1ef
Zafer Balkan <zafer@zaferbalkan.com>
2021-10-12 15:21:45 +0300

    Completed task 3

--- src/main.py
+++ src/main.py
@@ -7,11 +7,13 @@ if __name__ == '__main__':
         from crypto.transmissionCipher import TransmissionCipher
         from crypto.vigenereCipher import VigenereCipher
         from crypto.indexOfCoincidence import IndexOfCoincidence
+        from crypto.affineCipher import AffineCipher
     else:
         from .crypto.caesarCipher import CaesarCipher
         from .crypto.transmissionCipher import TransmissionCipher
         from .crypto.vigenereCipher import VigenereCipher
         from .crypto.indexOfCoincidence import IndexOfCoincidence
+        from .crypto.affineCipher import AffineCipher
 
 #region Tasks
 def task_1(caesar : CaesarCipher, transmission : TransmissionCipher) -> None:
@@ -35,6 +37,14 @@ def task_2(vigenere : VigenereCipher, ioc :IndexOfCoincidence) -> None:
     print('2. ' , ioc.calculate(text= plaintext))
     print('3. ' , ioc.calculate(text= ciphertext))
 
+def task_3(affine : AffineCipher) -> None:
+    plaintext = 'SURFACE'
+    ciphertext = 'NJCAXTP'
+
+    print('Task 3:')
+    print('1. Encryption key: ', affine.calculate_encryption_key(plain_text= plaintext, cipher_text= ciphertext))
+    print('2. Decryption key: ', affine.calculate_decryption_key(plain_text= plaintext, cipher_text= ciphertext))
+
 #endregion
 
 def main():
@@ -42,9 +52,11 @@ def main():
     transmission  = TransmissionCipher()
     vigenere = VigenereCipher()
     ioc = IndexOfCoincidence()
+    affine = AffineCipher()
 
     task_1(caesar= caesar, transmission= transmission)
     task_2(vigenere= vigenere, ioc= ioc)
+    task_3(affine= affine)
 
 if __name__ == "__main__":
     main()

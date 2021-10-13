32a05c28dc8cd146ba9134fa31eaea2fc9288f79
Zafer Balkan <zafer@zaferbalkan.com>
2021-10-13 12:54:58 +0300

    Instead of printing to console, generated LaTex

--- src/main.py
+++ src/main.py
@@ -17,45 +17,102 @@ if __name__ == '__main__':
         from .crypto.affineCipher import AffineCipher
         from .crypto.oneTimePad import OneTimePad
 
-#region Tasks
-def task_1(caesar : CaesarCipher, transmission : TransmissionCipher) -> None:
-    plaintext = "BLOCKCHAIN"
+from pylatex import Document, Section, Subsection, Math,PageStyle, Head, MiniPage, Foot, LargeText, MediumText, simple_page_number, LineBreak
+from pylatex.utils import italic, bold
+from datetime import date
 
-    temp = caesar.encrypt(plain_text= plaintext, key=9) # S1
+def generate_latex(caesar : CaesarCipher, transmission : TransmissionCipher, vigenere : VigenereCipher, ioc :IndexOfCoincidence, affine : AffineCipher, otp : OneTimePad) -> None:
+    geometry_options = {"margin": "2cm"}
+    doc = Document(geometry_options=geometry_options)
+
+    # Add document header
+    header = PageStyle("header")
+
+    # Create left header
+    with header.create(Head("L")):
+        header.append("Page date: ")
+        header.append(date.today().strftime("%B %d, %Y"))
+
+    # Create right header
+    with header.create(Head("R")):
+        header.append("Zafer BALKAN - 212289IVCM")
+
+    # Create right footer
+    with header.create(Foot("R")):
+        header.append(simple_page_number())
+
+    doc.preamble.append(header)
+    doc.change_document_style("header")
+
+     # Add Heading
+    with doc.create(MiniPage(align='c')):
+        doc.append(LargeText(bold("1st Assignment")))
+        doc.append(LineBreak())
+        doc.append(MediumText(bold("ITC8240 CRYPTOGRAPHY - Autumn 2021")))
+
+    # Add body
+    with doc.create(Section('Answers')):
+        task_1(caesar= caesar, transmission= transmission, doc= doc)
+        task_2(vigenere= vigenere, ioc= ioc, doc= doc)
+        task_3(affine= affine, doc= doc)
+        task_4(otp= otp, doc= doc)
+        task_5(doc= doc)
+
+    doc.generate_pdf('docs/1st Assignment', clean_tex=False)
+
+#region Tasks
+def task_1(caesar : CaesarCipher, transmission : TransmissionCipher, doc : Document) -> None:
+    plain_text = "BLOCKCHAIN"
+
+    temp = caesar.encrypt(plain_text= plain_text, key=9) # S1
     temp = transmission.encrypt(plain_text= temp, key= '51324',padding='x') # P1
     temp = caesar.encrypt(plain_text= temp, key= 19) # S2
-    ciphertext = transmission.encrypt(plain_text= temp, key='31425', padding='x') # P2
+    cipher_text = transmission.encrypt(plain_text= temp, key='31425', padding='x') # P2
 
-    print('Task 1:')
-    print(ciphertext)
+    with doc.create(Subsection('Task 1')):
+        doc.append("Ciphertext: ")
+        doc.append(cipher_text)
 
-def task_2(vigenere : VigenereCipher, ioc :IndexOfCoincidence) -> None:
-    plaintext = 'FRIENDSMAKETHEWORSTENEMIES'
+def task_2(vigenere : VigenereCipher, ioc :IndexOfCoincidence, doc: Document) -> None:
+    plain_text = 'FRIENDSMAKETHEWORSTENEMIES'
     key = 'list'
 
-    ciphertext = vigenere.encrypt(plain_text= plaintext, key= key)
-    print('Task 2:')
-    print('1. ' , ciphertext)
-    print('2. ' , ioc.calculate(text= plaintext))
-    print('3. ' , ioc.calculate(text= ciphertext))
+    cipher_text = vigenere.encrypt(plain_text= plain_text, key= key)
 
-def task_3(affine : AffineCipher) -> None:
-    plaintext = 'SURFACE'
-    ciphertext = 'NJCAXTP'
+    with doc.create(Subsection('Task 2')):
+        doc.append("1. Ciphertext: ")
+        doc.append(cipher_text)
+        doc.append("\n2. IoC of plain text: ")
+        doc.append(ioc.calculate(text= plain_text))
+        doc.append("\n3. IoC of cipher text: ")
+        doc.append(ioc.calculate(text= cipher_text))
 
-    print('Task 3:')
-    print('1. Encryption key: ', affine.calculate_encryption_key(plain_text= plaintext, cipher_text= ciphertext))
-    print('2. Decryption key: ', affine.calculate_decryption_key(plain_text= plaintext, cipher_text= ciphertext))
+def task_3(affine : AffineCipher, doc: Document) -> None:
+    plain_text = 'SURFACE'
+    cipher_text = 'NJCAXTP'
 
-def task_4(otp : OneTimePad) -> None:
+    with doc.create(Subsection('Task 3')):
+        doc.append("1. Encryption key: ")
+        doc.append(Math(data=[affine.calculate_encryption_key(plain_text= plain_text, cipher_text= cipher_text)]))
+        doc.append("\n2. Decryption key: ")
+        doc.append(Math(data=[affine.calculate_decryption_key(plain_text= plain_text, cipher_text= cipher_text)]))
+Math(data=['2*3', '=', 9])
+
+def task_4(otp : OneTimePad, doc: Document) -> None:
     key = otp.calculate_key(plain_text= 'DOUGH', cipher_text= '1000000110001010001000100')
     binary_key = otp.encode_binary(plain_text= key)
     cipher_text = otp.encrypt(plain_text= 'GLORY', binary_key= binary_key)
 
-    print('Task 4:')
-    print(cipher_text)
+    with doc.create(Subsection('Task 4')):
+        doc.append("Ciphertext: ")
+        doc.append(cipher_text)
 
+def task_5(doc: Document) -> None:
+    with doc.create(Subsection('Bonus Task:')):
+        doc.append("//TODO")
 #endregion
+
+
 
 def main():
     caesar = CaesarCipher()
@@ -65,10 +122,7 @@ def main():
     affine = AffineCipher()
     otp = OneTimePad()
 
-    task_1(caesar= caesar, transmission= transmission)
-    task_2(vigenere= vigenere, ioc= ioc)
-    task_3(affine= affine)
-    task_4(otp= otp)
+    generate_latex(caesar= caesar, transmission= transmission, vigenere= vigenere, ioc= ioc, affine= affine, otp= otp)
 
 if __name__ == "__main__":
     main()
